0711eb194095e4723b295e474f9e0a4d464d6987
HyoJoon <40314883+HyoJoonLouis@users.noreply.github.com>
2024-06-03 19:37:21 +0900

    123

--- Source/TeleportHoo/UI/RoundResultWidget.cpp
+++ Source/TeleportHoo/UI/RoundResultWidget.cpp
@@ -6,4 +6,5 @@ void URoundResultWidget::ShowResult(int Round, bool isWin)
 {
 	RoundText->SetText(FText::FromString(FString::Printf(TEXT("%i 라운드"),Round)));
 	ResultText->SetText(FText::FromString(isWin ? TEXT("승리") : TEXT("패배")));
+	PlayAnimationForward(ResultAnimation);
 }
